e4d12f2eb58479c8ff378832c74cfd35c60d3dc7
award2025 <aw145620@ohio.edu>
2023-01-31 16:47:34 -0500

    Variables added

--- invoice.cc
+++ invoice.cc
@@ -2,7 +2,9 @@
  *   @file: invoice.cc
  * @author: Arthur Ward
  *   @date: 1/31/2023
- *  @brief: Add Description
+ *  @brief: Program ask user the monitor type and quantity. It also asks user if they live in Ohio
+ * and will either give a discount or add taxes onto the total. 
+ * The program then provides a total price of the sale.
  */
 
 #include <iostream>
@@ -13,7 +15,20 @@ using namespace std;
 ///Constants and function prototypes
 
 int main(int argc, char const *argv[]) {
+    double total;
+    char liveOhio = ''; // will either take in y or n if user lives in ohio
+    char Samsung = 's';
+    char ViewSonic = 'v';
 
-    /*add code*/
+
+
+
+
+
+
+
+
+
+
     return 0;
 } /// main
