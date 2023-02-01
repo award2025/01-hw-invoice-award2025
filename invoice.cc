0fc493c9fddd4fe995a9250d8742cb57d8a36db6
root <root@DESKTOP-QV6232U>
2023-01-31 21:22:50 -0500

    Smoothed the program out and tested to see if correct values were displayed

--- invoice.cc
+++ invoice.cc
@@ -15,21 +15,27 @@ using namespace std;
 ///Constants and function prototypes
 
 int main(int argc, char const *argv[]) {
-    double total;
-    int liveOhio;
-    char computerType;
-    int quantity;
-    double price; 
+
+    double total; //total price of everything
+    int liveOhio; // stores value 1 if user lives in ohio, 2 if not
+    char computerType; //either Samsung or ViewSonic
+    int quantity; //quantity of computers
+    double price; //price of computer depending on brand selected
+    double tax;
+    double subtotal;
+    double discount; 
+    double totalPrice;
+    double reduction; // discount %
 
 cout << "Enter a monitor type (s, v) followed by the quantity: ";
 cin >> computerType;
 cin >> quantity;
 cout << "Do you live in Ohio (1 for yes/ 2 for no)? ";
 cin >> liveOhio;
-cout << "Monitors Invoice" << "_________________" << endl;
+cout << "Monitors Invoice" << endl << "_________________" << endl;
 cout << "Monitor brand: ";
 
-if(computerType == 's')
+if(computerType == 's') // determines price based on brand selected
 {
     cout << "Samsung" << endl;
     price = 190.00;
@@ -38,21 +44,43 @@ if(computerType == 's')
     cout << "ViewSonic" << endl;
     price =170.00;
 }
+
     
 cout << "Quantity: " << quantity << endl;
+cout << setw(4) << fixed << setprecision(2) << fixed;
 cout << "Price: $" << price << endl;
 cout << "Ohio Resident: ";
 
-if(liveOhio ==1)
+if(liveOhio ==1) //Gives output yes if user lives in Ohio, no if not and sets taxes accordingly
 {
     cout << "Yes" << endl;
+    tax = .07;
 }else
 {
     cout << "No" << endl;
 }
 
-double totalPrice = price*quantity;
+ if(quantity >= 3)// 5% discount if 3 or monitors was chosen
+{
+    reduction = .05;
+}
+   
+    totalPrice = price * quantity;
+    discount = reduction * totalPrice; 
+    subtotal = totalPrice - discount; 
+    tax = subtotal * tax; 
+    total = tax + subtotal;
+
 cout << "Total price: $" << totalPrice;
+cout << endl << "_________________" << endl;
+cout << "Discount: $" << discount << endl;
+cout << "Subtotal: $" << subtotal << endl;
+cout << "Taxes: $" << tax;
+cout << endl << "_________________" << endl;
 cout << "_________________" << endl;
+cout << "Total: $" << total;
+cout << endl << "_________________" << endl;
+cout << "_________________" << endl;
+
     return 0;
 } /// main
